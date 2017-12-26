8d5d4091e24b915128d155d58442af2ed200add2
LorenzoLim <renzo-lim@hotmail.com>
2017-12-26 21:19:24 +1000

    Spacing Added

--- database.js
+++ database.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const db = mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds125126.mlab.com:25126/messages`, {useMongoClient:true});
 
 const Schema = mongoose.Schema;
-
+ 
 mongoose.Promise = Promise
-   
+
 const Message = mongoose.model('Message', {
   id: Number,
   username: String,
